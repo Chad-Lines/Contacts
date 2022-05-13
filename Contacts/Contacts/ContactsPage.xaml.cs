b55ba70b9ac3f50e3579af507a6591685f2411d5
Chad <chad.lines1@gmail.com>
2022-05-13 08:59:09 -0600

    0.011 - Reading from the Database

--- Contacts/Contacts/ContactsPage.xaml.cs
+++ Contacts/Contacts/ContactsPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Contacts.Classes;
+using SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -21,6 +22,26 @@ namespace Contacts
         {
             // When the NEW button is clicked, navigate to the MainPage
             Navigation.PushAsync(new MainPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            // Every time the Contacts Page is displayed, this code will run
+
+            base.OnAppearing();         // Inheriting from the base class
+
+            // Connecting to the SQLite database 
+            using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
+            {
+                // Keep in mind that, with SQLite, if you try to create a table that already
+                // exists, it simply ignores the execution. That's why we can "create" this
+                // table in multiple places (as a fail-safe) without creating redundant 
+                // tables.
+                conn.CreateTable<Contact>();                    // Creating the Database
+                var contacts = conn.Table<Contact>().ToList();  // Converting the database table into a list
+
+            }
+            
         }
     }
 }
